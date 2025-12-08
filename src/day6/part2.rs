874884bdec0a57ad3cf9d13b6bb085d2d4a85fbf
nikpan1 <96376932+nikpan1@users.noreply.github.com>
2025-12-09 00:43:19 +0100

    day6 part2

--- src/day6/part2.rs
+++ src/day6/part2.rs
@@ -4,9 +4,10 @@ pub fn read_file(path: &str) -> String {
     read_to_string(path).expect("Reading file failed.")
 }
 
+#[derive(Clone)]
 struct Number {
-    number: u8,
-    number_index: u128,
+    number: u128,
+    line_index: u8,
 }
 
 #[cfg(test)]
@@ -16,51 +17,75 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let mut values: Vec<String> = read_file("src/day6/input.txt").lines()
-            .map(|x| x.to_string())
-            .collect();
+        let mut lines: Vec<String> = read_input_as_lines();
+        let operators_line = lines.pop().unwrap();
+        let operators: Vec<&str> = operators_line.split_whitespace().collect();
 
-        let operations: String = values.pop().unwrap();
-        let operation_indexes: Vec<usize> = operations.chars()
-            .enumerate()
-            .filter(|(_, op)| *op != ' ')
-            .map(|(i, _)| i)
-            .collect();
-
-        let mut ops: HashMap<usize, Vec<Number>> = HashMap::new();
-        for mut value in values {
-            value  += " ";
-            let mut line = value.chars();
-
-            let mut op_index = 0;
-            for i in 0..value.len() {
-                let current = line.nth(i).unwrap();
+        let mut equations: HashMap<usize, HashMap<usize, Vec<Number>>> = HashMap::new();
+        for (l_idx, line) in lines.iter().enumerate() {
+            let chars: Vec<char> = line.chars().collect();
+            let mut op_idx = 0;
+            for c_idx in 0..line.len() {
+                let current = chars[c_idx];
                 if current != ' ' {
-                    ops.entry(op_index).or_insert_with(Vec::new).push(Number {
-                        number: current as u8,
-                        number_index: i as u128,
+                    equations.entry(op_idx)
+                        .or_insert_with(HashMap::new)
+                    .entry(c_idx)
+                        .or_insert_with(Vec::new)
+                    .push(Number {
+                        number: current.to_digit(10).unwrap_or(0) as u128,
+                        line_index: l_idx as u8,
                     });
-                    let next = line.nth(i + 1 );
-                    match next {
-                        None => println!("end"),
-                        Some(n) => if n == ' ' {
-                            op_index += 1;
-                        },
+
+                    if isEndOfNumber(line, &chars, c_idx) {
+                        op_idx += 1;
                     }
                 }
             }
         }
+        let mut res = 0;
+        for (op_idx, operator) in operators.iter().enumerate() {
+            let equation = equations.get(&op_idx).unwrap();
+            let numbers: Vec<u128> = equation.iter()
+                .map(|(_, vec)| {
+                    let mut ordered: Vec<Number> = (*vec).clone();
+                    ordered.sort_by(|a, b| {a.line_index.cmp(&b.line_index)});
 
+                    let size = ordered.len();
+                    ordered.iter().enumerate()
+                        .map(|(idx, value)| {
+                            let power = (size - 1 - idx) as u32;
+                            value.number * 10u128.pow(power)
+                        })
+                        .sum()
+                }).collect();
 
+            let equation_result = if *operator == "*" {
+                let mut result = 1;
+                for v in numbers {
+                    result *= v;
+                }
+                result
+            } else if *operator == "+" {
+                let mut result = 0;
+                for v in numbers {
+                    result += v;
+                }
+                result
+            } else { panic!("panic") };
+            res += equation_result;
+        }
 
-        // iteruj znak po znaku i patrz
-        // jak znak != " " to znaczy ze chcemy dodac wartosc do mapy
-        // jesli current == " " oraz previous != " " to znaczy ze inkrementujemy wartosc klucza
-        //
-        // w sumie to nie musi byc mapa, po otrzymaniu operaions.len() mozemy prealokacje zrobic Vec<Vec<Number>>
-        //
-        // po tym wszystkim liczymy max index i od niego zaczynamy (bierzmey wszystkie o tym indeksie i sortujemy po linen umber i otrzymujemy nasza liczbe
+        assert_eq!(res, 11136895955912);
+    }
 
-        assert_eq!(1, 1);
+    fn isEndOfNumber(line: &String, chars: &Vec<char>, c_idx: usize) -> bool {
+        c_idx < line.len() - 1 && *chars.get(c_idx + 1).unwrap() == ' '
+    }
+
+    fn read_input_as_lines() -> Vec<String> {
+        read_file("src/day6/input.txt").lines()
+            .map(|x| x.to_string())
+            .collect()
     }
 }
